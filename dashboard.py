bbe033b557ca2cf6fb70efce1d153ccf7a361215
David Egbert <degbert@KBPDEVDEGBE1018.local>
2017-10-10 15:09:40 -0400

    more setup

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,24 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, session, request, redirect, url_for
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 app.config['SECRET_KEY'] = 'change to environ variable later'
+
+@app.route('/')
+def future_dash():
+    test_message = 'This is a test!'
+    return render_template('future_dash.html', the_test=test_message)
+
+# @app.route('/will_not_hit', methods=['GET', 'POST'])
+# def index():
+#     form = {}
+#     if form.validate_on_submit():
+#         old_name = session.get('name')
+#         session['name'] = form.name.data
+#         form.form_name.data = ''
+#         return redirect(url_for('index'))
+#     return render_template('index.html', form=form, name=session.get('name'))
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
